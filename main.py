2a91d3f6817475ce559e1964dc1405f3fb6ee473
Manali1344 <108320924+Manali1344@users.noreply.github.com>
2026-02-24 17:49:50 +0530

    Modify CORS settings to allow all domains
    Updated CORS middleware to allow all origins for testing.

--- main.py
+++ main.py
@@ -9,17 +9,11 @@ app = FastAPI()
 # ✅ CORS — THIS IS THE KEY FIX
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://connplexlive.in",
-        "https://www.connplexlive.in",
-        "https://manali1344.github.io",
-        "https://connplexlive.github.io/",
-    ],
+    allow_origins=["*"],   # ✅ Allow all domains (temporary testing)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-SYMBOL = "CONNPLEX"
 
 session = requests.Session()
 HEADERS = {
